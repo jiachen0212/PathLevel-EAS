6c47a865bf8497e7061a6395f76a93911c23a7ba
jiachen0212 <jiachen0212@163.com>
2018-07-19 18:50:55 +0800

    Update distributed.py
    emm.. at the line 88, we need to  modify the pythonpath according to our own environment.....

--- code/CIFAR/expdir_monitor/distributed.py
+++ code/CIFAR/expdir_monitor/distributed.py
@@ -85,7 +85,7 @@ class RemoteController:
 	
 	@property
 	def exe_cmd(self):
-		return 'CUDA_VISIBLE_DEVICES={gpuid} /..path to ypur python/  {executive}'.format(
+		return 'CUDA_VISIBLE_DEVICES={gpuid} /..path to your python/  {executive}'.format(
 			executive=self.executive,
 			gpuid=self.gpuid
 		)
